ff18b93134ba4ff812863a923780df5d4637a434
David Yusta <david.yusta@guidesmiths.com>
2020-08-26 18:36:03 +0200

    feat: add custom http hook

---
+++ src/hooks/http.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+export const useHttp = (url, dependencies) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedData, setFetchedData] = useState(null);
+
+    //fetch('https://swapi.dev/api/people')
+    useEffect(() => {
+        setIsLoading(true);
+        console.log('Sending HTTP request to ' + url);
+        fetch(url)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Failed to fetch.');
+            }
+            return response.json();
+          })
+          .then(data => {
+            setIsLoading(false);
+            setFetchedData(data);
+          })
+          .catch(err => {
+            setIsLoading(false);
+            console.log(err);
+          });
+    }, dependencies);
+
+    return [isLoading, fetchedData];
+};
